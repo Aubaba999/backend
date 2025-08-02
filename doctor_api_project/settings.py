47bcb0d694a276ee74d0cc0fe0d2b0828461de22
Aubaba999 <sutjii1989@gmail.com>
2025-08-02 23:20:27 +0700

    13

--- doctor_api_project/settings.py
+++ doctor_api_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -97,12 +98,19 @@ WSGI_APPLICATION = 'doctor_api_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    # ถ้ามี (กำลังรันบน Railway) ให้ใช้ฐานข้อมูลของ Railway
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
     }
-}
+else:
+    # ถ้าไม่มี (กำลังรันบนเครื่องตัวเอง) ให้ใช้ SQLite เหมือนเดิม
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
