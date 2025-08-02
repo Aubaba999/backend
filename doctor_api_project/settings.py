232bc74d2836f9e7af7359ffc74462bfc72fcf0b
Aubaba999 <sutjii1989@gmail.com>
2025-08-03 01:45:01 +0700

    19

--- doctor_api_project/settings.py
+++ doctor_api_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     'web-production-9270.up.railway.app',
